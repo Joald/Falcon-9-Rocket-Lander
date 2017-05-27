05b23f7d5ef36282f76ed9ee91311277a9a5f68c
Joald <jacek.olczyk98@gmail.com>
2017-05-27 08:48:44 +0200

    Made background prettier

---
+++ Assets/BackgroundScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour {
+
+    void Resize()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        Vector3 widthX = transform.localScale;
+        widthX.x = worldScreenWidth / width;
+        transform.localScale = widthX;
+        Vector3 yHeight = transform.localScale;
+        yHeight.y = worldScreenHeight / height;
+        transform.localScale = yHeight;
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        Resize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
